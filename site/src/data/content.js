c31662f31aebdc43b4e1eafbc559d1814413e1c2
root <root@ip-172-31-35-230.eu-central-1.compute.internal>
2026-06-07 16:51:09 +0000

    TG Bot: ich will die emailadresse auf info@musikschule-cms-bielefeld.de wechseln

--- site/src/data/content.js
+++ site/src/data/content.js
@@ -245,7 +245,7 @@ export const content = {
             "33719 Bielefeld"
         ],
         "phone": "+49 521 3367416",
-        "email": "juri.klassen@gmx.de",
+        "email": "info@musikschule-cms-bielefeld.de",
         "form": {
             "nameLabel": "Ihr Name *",
             "emailLabel": "Ihre E-Mail-Adresse *",
